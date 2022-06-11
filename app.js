57300017bc9a3c9e847413f29eee858c960b6914
FleurLis <53279500+DungeonCR@users.noreply.github.com>
2022-06-11 16:52:04 +0800

    Update app.js

--- app.js
+++ app.js
@@ -5,3 +5,18 @@ input.addEventListener('keyup', (e) => {
         window.open('https://nhentai.net/g/' + e.target.value, '_blank');
     }
 })
+
+
+
+var websites = new Array(
+	"http://www.hurriyet.com",
+	"http://www.milliyet.com",
+	"http://www.amazon.com"
+);
+var counter = 0;
+var sTimeOut = setInterval(function () {
+    var index = counter%(websites.length);
+    
+    $("#website_div").html($('<iframe src="' + websites[index] + '" width="500" height="500" border="0"/>' ));
+    counter++;	
+}, 5000);
